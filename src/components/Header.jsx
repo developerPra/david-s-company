a337ce712a7dd309afdd70b9312409bfbf0c3f4f
Prasad Lokhande <developerPra21@gmail.com>
2021-11-19 02:06:23 +0530

    setup installed

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/header.scss"
+
+const Header = () => {
+    return (
+    <div className="flex">
+        <h1>This is First Commit </h1>
+        <h2>This is flex traisl</h2>
+    </div> 
+    ) 
+}
+export default Header;
